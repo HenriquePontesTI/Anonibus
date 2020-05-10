2b26452516d3b2276607c574de831f973f34f98d
Henrique Marques Lopes de Pontes <henrique.pontes@radixeng.com.br>
2020-05-10 15:57:26 -0300

    ajustes de mensagens

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Ionicons } from '@expo/vector-icons'
 import {
   StyleSheet, Text, View, YellowBox, TouchableOpacity,
-  TextInput, Image
+  TextInput, Image, ScrollView
 } from 'react-native';
 import firebase from './config/firebase';
 import api from './services/axios';
@@ -12,8 +12,11 @@ YellowBox.ignoreWarnings(['Setting a timer']);
 
 export default function App() {
 
+  const db = firebase.firestore();
   const [caixaTexto, setCaixaTexto] = useState('');
   const [user, setUser] = useState(null);
+  const [mensagens, setMensagens] = useState([]);
+  const [scrollView, setScrollView] = useState(null);
 
   const salvar = () => {
     api.post('/enviarMensagem', {
@@ -21,15 +24,32 @@ export default function App() {
       usuario: user.name,
       avatar: user.picture
     }).then(function () {
+      setMensagens([...mensagens, caixaTexto]);
       console.log('salvo')
     }).catch(function (err) {
       console.log(err);
-
     })
   }
 
   useEffect(() => {
     carregaUsuarioAnonimo();
+    let mensagens_enviadas = []
+    const unsubscribe = db.collection("chats")
+      .doc("sala_01").collection('mensagens')
+      .onSnapshot({ includeMetadataChanges: false }, function (snapshot) {
+        snapshot.docChanges().forEach(function (change) {
+          if (change.type === "added") {
+            const { mensagem, usuario, avatar } = change.doc.data()
+            const id = change.doc.id
+            mensagens_enviadas.push({ mensagem, usuario, avatar, id })
+          }
+        })
+        setMensagens([...mensagens_enviadas])
+        scrollView ? scrollView.scrollToEnd({ animated: true }) : null;
+      })
+    return () => {
+      unsubscribe()
+    }
   }, [])
 
   const carregaUsuarioAnonimo = () => {
@@ -77,6 +97,35 @@ export default function App() {
         onChangeText={text => setCaixaTexto(text)}
         value={caixaTexto}
       />
+      <ScrollView
+        ref={(view) => { setScrollView(view) }}
+        style={styles.scrollView}>
+        {
+          mensagens.length > 0 && mensagens.map(item => (
+            <View key={item.id} style={{
+              flexDirection: 'row',
+              paddingLeft: 10,
+              paddingTop: 10,
+              marginRight: 60
+            }}>
+              <Image style={{
+                margin: 5,
+                width: 40,
+                height: 40,
+                borderWidth: 1,
+                borderColor: "#333",
+                borderRadius: 50
+              }}
+                source={{ uri: item.avatar }} />
+              <View style={{ marginTop: 5, flexDirection: 'column' }}>
+                <Text style={{ fontSize: 12, color: '#999' }} >{item.usuario}</Text>
+                <Text>{item.mensagem}</Text>
+              </View>
+
+            </View>
+          ))
+        }
+      </ScrollView >
       <TouchableOpacity onPress={salvar}>
         <Ionicons style={{ margin: 3, color: '#f34f' }} name="md-send" size={32} color={'#999'} />
       </TouchableOpacity>
