3c46132cd66ba8cc90da35f3dbf1fc72669cd092
Henrique Marques Lopes de Pontes <henrique.pontes@radixeng.com.br>
2020-06-02 17:54:13 -0300

    geolocalização do usuário

--- Profile/index.js
+++ Profile/index.js
@@ -1,16 +1,25 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import { View, Text, StyleSheet, Button, PermissionsAndroid } from "react-native";
 import { } from './styles';
 
 import { AuthContext } from '../context';
 
 import firebase from 'firebase';
+import flagBlueImg from './assets/flag-blue.png';
+import flagPinkImg from './assets/flag-pink.png';
 
 
 
 export default Profile = () => {
 
-  const { signOut } = React.useContext(AuthContext)
+  const [location, setlocation] = useState([]);
+  const [latitude, setlatitude] = useState(null);
+  const [longitude, setlongitude] = useState(null);
+
+  const { signOut } = React.useContext(AuthContext);
 
   const handleSignOut = () => {
     firebase.auth().signOut().then(function () {
@@ -21,8 +30,51 @@ export default Profile = () => {
     });
   }
 
+  const getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+    let location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Highest });
+    setlatitude(location.coords.latitude);
+    setlongitude(location.coords.longitude);
+    console.log(latitude, longitude)
+
+  };
+
+  console.disableYellowBox = true;
+
+
   return (
+    
     <View style={styles.container}>
+      <Text>VOCÊ ESTÁ AQUI: </Text>
+      <Button title="Localização" onPress={() => getLocationAsync()} />
+      {latitude &&
+        <>
+          <MapView style={styles.mapStyle} initialRegion={{
+            latitude: latitude,
+            longitude: longitude,
+            latitudeDelta: 1,
+            longitudeDelta: 1
+          }}>
+            <Marker onPress={() => getLocationAsync()}
+              coordinate={{
+                latitude: latitude ,
+                longitude: longitude,
+              }}
+              centerOffset={{ x: -42, y: -60 }}
+              anchor={{ x: 0.84, y: 1 }}
+              opacity={0.6}
+              image={location ? flagBlueImg : flagPinkImg}
+            >
+              <Text style={styles.marker}>X</Text>
+            </Marker>
+          </MapView>
+        </>
+      }
       <Text>PROFILE</Text>
       <Button title="Sair" onPress={() => handleSignOut()} />
     </View>
@@ -34,6 +86,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  mapStyle: {
+    width: 400,
+    height: 400,
+  },
+  marker: {
+    marginLeft: 46,
+    marginTop: 33,
+    fontWeight: 'bold',
   },
   button: {
     paddingHorizontal: 20,
